331ac93e39a89aa9976a08f9d89aaf3d3033dff6
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2025-06-02 20:08:04 +0800

    Add files via upload

--- code/main.h
+++ code/main.h
@@ -6,10 +6,11 @@
 
 #include <getopt.h>
 
-static const char *short_options = "k:i:t:o:a:q";
+static const char *short_options = "h:k:i:t:o:a:q";
 
 static struct option long_options[] = {
         // general options
+        {"help",required_argument,0,'h'},
         {"kmer_length",required_argument,0,'k'},
         {"reads",required_argument,0,'i'},
         {"trunk_filename",required_argument, 0,'t'},
